79d6bdfb974e36726d85e343b61b466e8ec12a99
l.samko <liudmyla@backbase.com>
2022-07-07 12:08:15 +0300

    create binding between queue and exchange

--- src/main/java/com/example/rabbitmq/RabbitMQConfig.java
+++ src/main/java/com/example/rabbitmq/RabbitMQConfig.java
@@ -1,5 +1,10 @@
 package com.example.rabbitmq;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Binding.DestinationType;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -17,8 +22,26 @@ public class RabbitMQConfig {
     Queue myQueue() {
         return new Queue(MY_QUEUE, true);
     }
+
     @Bean
-    ConnectionFactory connectionFactory(){
+    Exchange myExchange() {
+        return ExchangeBuilder.topicExchange("MyTopicExchange")
+            .durable(true)
+            .build();
+    }
+
+    @Bean
+    Binding binding() {
+      //  return new Binding(MY_QUEUE, DestinationType.QUEUE, "MyTopicExchange", "topic", null);
+        return BindingBuilder
+            .bind(myQueue())
+            .to(myExchange())
+            .with("topic")
+            .noargs();
+    }
+
+    @Bean
+    ConnectionFactory connectionFactory() {
         CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
         connectionFactory.setUsername("guest");
         connectionFactory.setPassword("guest");
@@ -26,7 +49,7 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    MessageListenerContainer messageListenerContainer(){
+    MessageListenerContainer messageListenerContainer() {
         SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
         simpleMessageListenerContainer
             .setConnectionFactory(connectionFactory());
